49b191f7411e69bf37fc92da7d6b3db671dd1a34
Flavio Josué Castillo Farías <fcastilf@emeal.nttdata.com>
2024-02-11 10:20:23 -0500

    first commit yankee server

---
+++ src/main/java/com/nttdata/bootcamp/yankeemanagement/YankeeApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.yankeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YankeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YankeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/yankeemanagement/YankeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.yankeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YankeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
